9143f7b0378d4ec1e6067c4db5c4121283489d09
Maria-siopi33 <mariasiopi33@gmail.com>
2025-04-13 14:47:33 +0300

    jekiniste deitei ayto
    deite ayto

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 #include <cstdlib>
 #include <ctime>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-dokimi github
+#include <string>
 using namespace std;
+   void showDifficultyMenu() {
+    cout << "\nChoose difficullty:\n";
+    cout << "1. Easy\n";
+    cout << "2. Medium\n";
+    cout << "3. Hard\n";
+    cout << "ep: ";
+}
+int main(){
 
+    cout << "==============================\n";
+    cout << " Welcome to Sudoku!\n";
+    cout << "==============================\n";
+    int ep;//epilogi xrhsth
+    showDifficultyMenu();
+    cin >>ep;
+    switch (ep) {
+        case 1:
+            cout << "Easy level""\n";
+          
+            break;
+        case 2:
+            cout << "difficult level.\n";
+           
+            break;
+        case 3:
+            cout << "more difficult.\n";
+          
+            break;
+        default:
+            cout << "you didnt chose correctly.\n";
+            break;
+    }
+
+   return 0; 
+    
+    
+}
+    
+ 
