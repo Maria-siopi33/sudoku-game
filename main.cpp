a58f758f36c0995dffa415e12225848c6911ba0f
Michaleri <mariamixal05@gmail.com>
2025-05-14 02:21:41 +0300

    rapunzel
    mikres allages

--- main.cpp
+++ main.cpp
@@ -5,56 +5,31 @@
 #include <vector>
 #include <algorithm>
 #include <chrono>
-#include <sstream>
-
+//lol
 using namespace std;
-
+int C = 0;
 const int SIZE = 9;
 
+void instructions(){
+	cout<<"Accordingly to the level of difficulty you will choose, we offer you some points to purchase a help if you need it.\n";
+	cout<<"Levels:\n";
+	cout << "1. Easy: 10 points\n";
+    cout << "2. Medium: 20 points\n";
+    cout << "3. Hard: 30 points\n";
+    cout<<"With every number you find you win 2 additional points. \n";
+    cout<<"With every mistake you do you lose 2 points from your total. \n";
+}
+
 void showDifficultyMenu() {
-    cout << "Choose difficulty:\n";
-    cout << "1. Easy \n";
-    cout << "2. Medium \n";
-    cout << "3. Hard \n";
+    cout << "\nChoose difficulty:\n";
+    cout << "1. Easy\n";
+    cout << "2. Medium\n";
+    cout << "3. Hard\n";
     cout << "0. Exit\n";
+    cout << "Your choice: ";
 }
 
-void giveHint(int board[SIZE][SIZE], int solutionBoard[SIZE][SIZE]) { //dinei ena tyxaio hint 
-    vector<pair<int, int>> emptyCells;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == 0) {
-                emptyCells.push_back({i, j});
-            }
-        }
-    }
-
-    if (!emptyCells.empty()) {
-        auto hintCell = emptyCells[rand() % emptyCells.size()];
-        int row = hintCell.first;
-        int col = hintCell.second;
-        cout << "Hint: Try placing a " << solutionBoard[row][col]
-             << " at row " << row + 1 << ", column " << col + 1 << ".\n";
-    } else {
-        cout << "No hints available. Board is full.\n";
-    }
-}
-
-void autoFillOneCell(int board[SIZE][SIZE], int solutionBoard[SIZE][SIZE]) { //dinei pragmatikh voitheia
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == 0) {
-                board[i][j] = solutionBoard[i][j];
-                cout << "Auto-filled cell at row " << i + 1 << ", column " << j + 1
-                     << " with value " << solutionBoard[i][j] << ".\n";
-                return;
-            }
-        }
-    }
-    cout << "Board is already full.\n";
-}
-
-void displayBoard(int board[SIZE][SIZE], int chances = -1,int hintsLeft = -1) {
+void displayBoard(int board[SIZE][SIZE], int mistakes = 0) {
     cout << "\n    1 2 3   4 5 6   7 8 9 \n";
     cout << " ---------------------------\n";
     for (int i = 0; i < SIZE; i++) {
@@ -72,15 +47,10 @@ void displayBoard(int board[SIZE][SIZE], int chances = -1,int hintsLeft = -1) {
         cout << "\n";
     }
     cout << " ---------------------------\n";
-    if (chances >= 0) {
-        cout << "Remaining chances: " << chances << "/3\n";
-    }
-     if (hintsLeft >= 0) {
-        cout << "Remaining hints: " << hintsLeft << "/2\n";
-    }
-    cout << " ---------------------------\n";
+    cout << "Mistakes: " << mistakes << "/3 | Points: " << C << "\n";
 }
-  
+
+
 
 bool isSafe(int board[SIZE][SIZE], int row, int col, int num) {
     for (int x = 0; x < SIZE; x++) {
@@ -142,77 +112,104 @@ bool isBoardFull(int board[SIZE][SIZE]) {
     return true;
 }
 
-void playSudoku(int board[SIZE][SIZE], int solutionBoard[SIZE][SIZE]) {
-    int chances = 3;
-    int hintsLeft = 2;
 
-    displayBoard(board, chances, hintsLeft);
-
-    while (!isBoardFull(board)) {
-        cout << "\nEnter 'h' for hint, 'a' for auto-fill, or 'row col num': ";
-        string inputLine;
-        getline(cin >> ws, inputLine);
-
-        if (inputLine == "h") {
-            if (hintsLeft > 0) {
-                giveHint(board, solutionBoard);
-                hintsLeft--;
-            } else {
-                cout << "No hints left.\n";
+bool giveHelp(int board[SIZE][SIZE], int solutionBoard[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            if (board[i][j] == 0) {
+                board[i][j] = solutionBoard[i][j];
+                cout << "Help used: Cell (" << i+1 << "," << j+1 << ") filled with " << solutionBoard[i][j] << ".\n";
+                return true;
             }
-            displayBoard(board, chances, hintsLeft);
-            continue;
-        } else if (inputLine == "a") {
-            if (hintsLeft > 0) {
-                autoFillOneCell(board, solutionBoard);
-                hintsLeft--;
-            } else {
-                cout << "No auto-fills left.\n";
-            }
-            displayBoard(board, chances, hintsLeft);
-            continue;
-        }
-
-        int row, col, num;
-        stringstream ss(inputLine);
-        if (ss >> row >> col >> num) {
-            row--; col--;
-
-            if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && num >= 1 && num <= 9) {
-                if (board[row][col] == 0) {
-                    if (solutionBoard[row][col] == num) {
-                        board[row][col] = num;
-                        displayBoard(board, chances, hintsLeft);
-                    } else {
-                        cout << "Wrong number! That’s not the correct value for this cell.\n";
-                        chances--;
-                        if (chances == 0) {
-                            cout << "No more chances. Game over.\n";
-                            break;
-                        }
-                        displayBoard(board, chances, hintsLeft);
-                    }
-                } else {
-                    cout << "Cell already filled.\n";
-                }
-            } else {
-                cout << "Invalid input range. Try again.\n";
-            }
-        } else {
-            cout << "Invalid input format. Try again.\n";
         }
     }
+    return false;
+}
 
-    if (isBoardFull(board)) {
-        cout << "Congratulations! You've completed the Sudoku!\n";
+void GamePoints(int board[SIZE][SIZE], int solutionBoard[SIZE][SIZE], int row, int col, int num){
+    if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && num >= 1 && num <= 9) {
+        if (board[row][col] == 0) {
+            if (solutionBoard[row][col] == num) {
+                C += 2;
+                cout << "You won two points with your correct answer. Total amount: " << C << "\n";
+            } else {
+                C -= 2;
+                cout << "You lost two points with your wrong answer. Total amount: " << C << "\n";
+            }
+        }
     }
-
-    cout << "\nSolution Board:\n";
-    displayBoard(solutionBoard);
 }
 
 
-int main() {
+
+void playSudoku(int board[SIZE][SIZE], int solutionBoard[SIZE][SIZE]) {
+    int row, col, num;
+    int mistakes=0;
+    
+    displayBoard(board, mistakes);
+
+    while (!isBoardFull(board)) {
+        cout << "\nEnter row (1-9), column (1-9), and number (1-9),[Enter 0 0 0 for exit]: ";
+        cin >> row >> col >> num;
+        
+        if (row == 0 && col == 0 && num == 0) {
+    		cout << "You exited the game.\n";
+    		break;
+		}
+
+
+        row--; 
+        col--;
+
+        if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && num >= 1 && num <= 9) {
+            if (board[row][col] == 0) {
+				GamePoints(board, solutionBoard, row, col, num);
+				if (solutionBoard[row][col] == num) {
+    				board[row][col] = num;
+    				displayBoard(board,mistakes);
+				} 
+				else {
+    				mistakes++;
+    				cout << "Mistakes:" << mistakes << "/3\n";
+    				displayBoard(board, mistakes);
+    				if (mistakes == 3) {
+    					cout << "No more chances. Game over.\n";
+    					break;
+					}
+				}
+			} 
+			else {
+            	cout << "Cell already filled.\n";
+			}
+    	}
+		else{
+           	cout << "Invalid input. Try again.\n";
+		}     	
+	}
+    	
+		if (isBoardFull(board)) 
+       		cout << "Congratulations! You've completed the Sudoku!\n";
+    	cout << "\nSolution Board:\n";
+    	displayBoard(solutionBoard);
+}
+
+void LevelPoints(int l){
+	if (l==1){
+		C=C+10;
+		//cout<< "You have " << C << "points.\n"; efoson emfanizontai oi pontoi mazi me ton pinaka autew tiw emaniseis mporoyme na tis afairesoume
+	}
+	else if (l==2){
+		C=C+20;
+		//cout<< "You have " << C << "points.\n";
+	}
+	else if (l==3){
+		C=C+30;
+		//cout<< "You have " << C << "points.\n";
+	}
+}
+
+
+ int main() {
     int ep;
     int board[SIZE][SIZE] = {0};
     int solutionBoard[SIZE][SIZE] = {0};
@@ -225,47 +222,62 @@ int main() {
     cout << "==============================\n";
     cout << " \tWelcome to Sudoku!\n";
     cout << "==============================\n";
+    instructions();
     showDifficultyMenu();
     cin >> ep;
-
+   
     while (ep < 0 || ep > 3) {
         cout << "Invalid input. Please enter a number between 0 and 3 (0 for exit): ";
         showDifficultyMenu();
         cin >> ep;
     }
 
-    if (ep == 0) {
+    LevelPoints(ep);
+    if (ep == 1) 
+        visibleNumbers = 36;
+    else if (ep == 2) 
+        visibleNumbers = 32;
+    else if (ep == 3) 
+        visibleNumbers = 26;
+    else {
         auto end = steady_clock::now();
-        auto duration = duration_cast<seconds>(end - start).count();
-        cout << "You exited the game.\n";
-        cout << "Time taken: " << duration << " seconds.\n";
-        return 0;
+		auto duration = duration_cast<seconds>(end - start).count();
+		int minutes = duration / 60;
+		int seconds = duration % 60;
+		cout << "You exited the game.\n";
+		if (minutes<1)
+			cout << "Time taken:" << seconds << " seconds.\n";//emfanisi tou xronoy se morfi mm:ss
+		else
+			cout << "Time taken:" << minutes << ":" << seconds << " minutes.\n";//emfanisi tou xronoy se morfi mm:ss	
+		return 0;
     }
-
-    visibleNumbers = (ep == 1) ? 36 : (ep == 2) ? 32 : 26;
+    
     fillSudoku(board);
-    copy(&board[0][0], &board[0][0] + SIZE * SIZE, &solutionBoard[0][0]);
+    copy(&board[0][0], &board[0][0] + SIZE * SIZE, &solutionBoard[0][0]); // Save full solution
     removeNumbers(board, visibleNumbers);
 
     switch (ep) {
-        case 1: cout << "\tEasy level.\n"; break;
-        case 2: cout << "\tMedium level.\n"; break;
-        case 3: cout << "\tHard level.\n"; break;
+        case 1:
+			cout << "\n\tEasy level.\n"; break;
+        case 2: 
+			cout << "\n\tMedium level.\n"; break;
+        case 3: 
+			cout << "\n\tHard level.\n"; break;
     }
 
+    
     playSudoku(board, solutionBoard);
-
+    
     auto end = steady_clock::now();
-    auto duration = duration_cast<seconds>(end - start).count();
+	auto duration = duration_cast<seconds>(end - start).count();
+	int minutes = duration / 60;
+	int seconds = duration % 60;
 
-    if (duration < 60) {
-        cout << "\nTime taken: " << duration << " seconds.\n";
-    } else {
-        int minutes = duration / 60;
-        int seconds = duration % 60;
-        cout << "\nTime taken: " << minutes << " minutes and " << seconds << " seconds.\n";
-    }
+	if (minutes < 1)
+    	cout << "Time taken: " << seconds << " seconds.\n";
+	else
+   		cout << "Time taken: " << minutes << ":" << seconds << " minutes.\n";
 
+	cout << "Your total points are: " << C << "\n";
     return 0;
 }
-
